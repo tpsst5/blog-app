9db7a71c42e7902d39db6732e714163d67721063
tpsst5 <tshea31@gmail.com>
2022-02-07 22:41:37 -0700

    Add tag support for version 2

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.springboot.blog.controller;
 
 import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.payload.PostDtoV2;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
 import com.springboot.blog.utils.AppConstants;
@@ -10,9 +11,11 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/posts")
+@RequestMapping()
 public class PostController {
 
     private PostService postService;
@@ -23,13 +26,13 @@ public class PostController {
 
     // create blog post
     @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping
+    @PostMapping("/api/v1/posts")
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
     // get all posts
-    @GetMapping
+    @GetMapping("/api/v1/posts")
     public PostResponse getAllPosts(
             @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
             @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
@@ -39,15 +42,33 @@ public class PostController {
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
-    // get post by id
-    @GetMapping("/{id}")
-    public ResponseEntity<PostDto> getPostById(@PathVariable(value = "id") long id) {
+    // get post by id - V1
+    @GetMapping("/api/v1/posts/{id}")
+    public ResponseEntity<PostDto> getPostByIdV1(@PathVariable(value = "id") long id) {
         return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+    // get post by id - V2
+    @GetMapping("/api/v2/posts/{id}")
+    public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable(value = "id") long id) {
+        PostDto postDto = postService.getPostById(id);
+        PostDtoV2 postDtoV2 = new PostDtoV2();
+        postDtoV2.setId(postDto.getId());
+        postDtoV2.setTitle(postDto.getTitle());
+        postDtoV2.setDescription(postDto.getDescription());
+        postDtoV2.setContent(postDto.getContent());
+        List<String> tags = new ArrayList<>();
+        tags.add("Java");
+        tags.add("Spring Boot");
+        tags.add("AWS");
+        postDtoV2.setTags(tags);
+
+        return ResponseEntity.ok(postDtoV2);
     }
 
     // update post by id
     @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("/{id}")
+    @PutMapping("/api/v1/posts/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(value = "id") long id) {
 
         PostDto postResponse = postService.updatePost(postDto, id);
@@ -57,7 +78,7 @@ public class PostController {
 
     // delete post by id
     @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/api/v1/posts/{id}")
     public ResponseEntity<String> deletePostById(@PathVariable(value = "id") long id) {
 
         postService.deletePostById(id);
--- src/main/java/com/springboot/blog/payload/PostDtoV2.java
+++ src/main/java/com/springboot/blog/payload/PostDtoV2.java
@@ -1,4 +1,29 @@
 package com.springboot.blog.payload;
 
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.List;
+import java.util.Set;
+
+@Data
 public class PostDtoV2 {
+    private long id;
+
+    // title should not be empty/null and have at least 2 characters
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least 2 characters")
+    private String title;
+
+    // post should not be empty/null and have at least 10 characters
+    @NotEmpty
+    @Size(min= 10, message = "Post description should have at least 10 characters")
+    private String description;
+
+    // content should not be empty/null
+    @NotEmpty
+    private String content;
+    private Set<CommentDto> comments;
+    private List<String> tags;
 }
